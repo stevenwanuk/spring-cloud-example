8c4d95540b464a8d5b94a7b1c89bfe6481df1e66
Steven Wan <swan@onetrust.com>
2019-01-16 16:27:14 +0000

    add spring-boot-admin & eureka

---
+++ dashboard/src/main/java/com/sven/sc/dashboard/DashboardApplication.java
@@ -0,0 +1,18 @@
+package com.sven.sc.dashboard;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+
+}
+
